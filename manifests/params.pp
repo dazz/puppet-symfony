01c4770d383744863f99ef5a08fb473d8a345962
Anne-Julia Scheuermann <annejulia@gmail.com>
2012-04-05 17:23:25 +0200

    download symfony2 standard project from git and remove the .git file

---
+++ manifests/params.pp
@@ -0,0 +1,24 @@
+class symfony::params {
+
+	$root_dir_name = $root_dir_name ? {
+        	 "" => "symfony",
+    	default => $root_dir_name,
+  	}
+  	
+  	$root_dir_path = $root_dir_path ? {
+        	 "" => "/tmp",
+    	default => $root_dir_path,
+  	}
+  	
+  	$root_dir = $root_dir ? {
+        	 "" => "${root_dir_path}/${root_dir_name}",
+    	default => $root_dir,
+  	}
+  	
+  	$download_url = $download_url ? {
+  		"" => "http://github.com/symfony/symfony-standard.git",
+  		default => $download_url,
+  	}
+  	
+  	
+}
